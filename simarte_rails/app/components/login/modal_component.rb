b091befa8bdb7f9b98b2ea5ce26e20649b9a67e1
Jaco Erasmus <jierasmus27@gmail.com>
2026-04-05 18:59:28 +0200

    feat: Enhance login modal component by adding alert message display and updating form styling for improved user feedback

--- simarte_rails/app/components/login/modal_component.rb
+++ simarte_rails/app/components/login/modal_component.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Login::ModalComponent < ViewComponent::Base
-  def initialize(title: "SimARTe Admin Login")
+  def initialize(title: "SimARTe Admin Login", alert: nil)
     @title = title
+    @alert = alert
+  end
+
+  def alert_message
+    @alert.presence || helpers.flash[:alert]
   end
 
   private

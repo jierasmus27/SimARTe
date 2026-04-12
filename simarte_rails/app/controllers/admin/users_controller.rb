075484ed697b3667a8ce950a1aeb5a17ad7de5a6
Jaco Erasmus <jierasmus27@gmail.com>
2026-04-12 11:44:42 +0200

    feat: Add update action for user management in admin users controller

--- simarte_rails/app/controllers/admin/users_controller.rb
+++ simarte_rails/app/controllers/admin/users_controller.rb
@@ -3,4 +3,16 @@ class Admin::UsersController < Admin::BaseController
     @services = Service.order(:name)
     @users = policy_scope(User).includes(:services, subscriptions: :service).order(:email)
   end
+
+  def update
+    user = authorize User.find(params.expect(:id))
+    user.update(user_params)
+    redirect_to admin_users_path, notice: "User updated successfully"
+  end
+
+  private
+
+  def user_params
+    params.expect(:user).permit(:role)
+  end
 end

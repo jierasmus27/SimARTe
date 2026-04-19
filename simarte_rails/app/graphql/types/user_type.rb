60f6c2ba718f189ee1ca31855b25d9778e2838d0
Jaco Erasmus <jierasmus27@gmail.com>
2026-04-19 19:06:17 +0200

    feat(graphql): add ServiceType and services field to UserType
    Introduced ServiceType for GraphQL, defining fields for id and name. Updated UserType to include a services field that retrieves associated services for the user.

--- simarte_rails/app/graphql/types/user_type.rb
+++ simarte_rails/app/graphql/types/user_type.rb
@@ -8,9 +8,14 @@ module Types
     field :last_name, String, null: false
     field :role, String, null: false
     field :subscriptions, [ Types::SubscriptionType ], null: false
+    field :services, [ Types::ServiceType ], null: false
 
     def subscriptions
       object.subscriptions.includes(:service)
+    end
+
+    def services
+      object.services
     end
   end
 end

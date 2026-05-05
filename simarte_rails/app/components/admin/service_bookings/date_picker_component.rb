57b3be87107e7678668750f7e71214c238850da6
Jaco Erasmus <jierasmus27@gmail.com>
2026-05-05 23:33:20 +0200

    feat(date-picker): enhance date picker component with dynamic day selection
    Refactored the date picker component to utilize dynamic arrays for unselectable and selectable days based on the current date. Updated the initialization of the selected date to default to the current date. Improved the logic for displaying days from the previous month and handling unselectable start days of the month.

--- simarte_rails/app/components/admin/service_bookings/date_picker_component.rb
+++ simarte_rails/app/components/admin/service_bookings/date_picker_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::ServiceBookings::DatePickerComponent < ViewComponent::Base
-  def initialize(selected_date: nil, field_name: :date)
+  def initialize(selected_date: Date.current, field_name: :date)
     @selected_date = selected_date
     @field_name = field_name
   end
@@ -9,4 +9,35 @@ class Admin::ServiceBookings::DatePickerComponent < ViewComponent::Base
   private
 
   attr_reader :selected_date, :field_name
+
+  def current_month
+    @current_month ||= selected_date.month
+  end
+
+  def current_year
+    @current_year ||= selected_date.year
+  end
+
+  def days_from_last_month
+    @days_from_last_month ||= (
+      selected_date.beginning_of_month.to_date -
+        (selected_date.beginning_of_month.wday + 1).days
+    )..selected_date.last_month.end_of_month
+  end
+
+  def unselectable_start_days_of_month
+    return [] unless selected_date.beginning_of_month == Time.current.beginning_of_month
+    return [] if Time.current.day == 1
+
+    @unselectable_start_days_of_month ||= selected_date.beginning_of_month.to_date..(Time.current.to_date - 1.day)
+  end
+
+  def unselectable_start_days
+    @unselectable_start_days ||= days_from_last_month.to_a + unselectable_start_days_of_month.to_a
+  end
+
+  def selectable_days_of_month
+    (selected_date.beginning_of_month..selected_date.end_of_month).to_a - unselectable_start_days
+  end
+
 end
